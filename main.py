3d2fe92e208bd9f03c8a716f38d20be979bb3fea
TwoTales <noahvincent62@yahoo.ca>
2025-08-10 18:38:56 -0600

    D: added player to the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
@@ -10,12 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # creating player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     # infinite game loop
     while(1):
         for event in pygame.event.get():    # reading events
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")                # filling the screen "black"
+        player.draw(screen)                 # drawing player to screen
         pygame.display.flip()               # refreshing the screen
         # running at 60 FPS maximum
         dt = clock.tick(60) / 1000
